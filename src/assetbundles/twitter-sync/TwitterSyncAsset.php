4b88e46d32797b968cc61a490ce33eaf3f3d6e62
Matthew Jones <matt@boxhead.io>
2023-01-05 13:58:38 +0000

    Update namespace for asset bundle
    Signed-off-by: Matthew Jones <matt@boxhead.io>

--- src/assetbundles/twitter-sync/TwitterSyncAsset.php
+++ src/assetbundles/twitter-sync/TwitterSyncAsset.php
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2018 Boxhead
  */
 
-namespace boxhead\twittersync\assetbundles\TwitterSync;
+namespace boxhead\twittersync\assetbundles;
 
 use Craft;
 use craft\web\AssetBundle;
